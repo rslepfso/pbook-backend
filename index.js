ac7ea823d87de749208e427f8aee94f8a3764884
rslepfso <rslepfso@gmail.com>
2023-09-13 11:59:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Home Page</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people<p>
+  <p>Current time is ${new Date().toLocaleString()}
+  `);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  persons = persons.filter((p) => p.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  person.id = Math.random() * 1000;
+  persons = persons.concat(person);
+
+  if (!person.name) {
+    return response.status(400).json({
+      error: "Name is missing",
+    });
+  }
+
+  if (!person.number) {
+    return response.status(400).json({
+      error: "Number is missing",
+    });
+  }
+
+  const duplicates = persons.filter(
+    (p) => p.name.toLowerCase() === person.name.toLowerCase()
+  );
+
+  if (duplicates.length > 0) {
+    return response.status(400).json({
+      error: "Name is already exists",
+    });
+  }
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
